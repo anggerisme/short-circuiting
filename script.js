125d5baf5e96b22727c27aefb4fa567d3da5e881
Angger Bayu Pangestu <kontak@angger.me>
2021-03-15 09:59:33 +0700

    update

---
+++ script.js
@@ -0,0 +1,34 @@
+// const rumahMakan = {
+//   nama: "Rumah makan berkah",
+//   lokasi: "Jalan merpati blok m",
+//   kategori: ["tradisional", "murah", "warteg"],
+//   makanan: ["soto", "gulai ikan", "rendang", "sate", "nasi goreng"],
+//   minuman: ["es teh", "es jeruk", "kopi"],
+//   jamBuka: {
+//     kam: {
+//       buka: 12,
+//       tutup: 22,
+//     },
+//     jum: {
+//       buka: 11,
+//       tutup: 23,
+//     },
+//     sab: {
+//       buka: 0,
+//       tutup: 24,
+//     },
+//   },
+
+//   mieRebus: function (sawi, ...bumbuLain) {
+//     console.log(sawi);
+//     console.log(...bumbuLain);
+//   },
+// };
+
+/* 3 Sifat mengenai logical operators
+1. Mengembalikan tipe data apapun 
+2. Menggunakan tipe data apapun
+3. Short circuiting evaluation*/
+console.log(3 || "bambang");
+console.log(3 && "bambang");
+console.log(null || undefined);
