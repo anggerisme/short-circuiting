c90610eeed16cb2298ca4fd32dbac0c0592fd125
Angger Bayu Pangestu <kontak@angger.me>
2021-03-15 10:52:17 +0700

    Contoh kasus

--- README.md
+++ README.md
@@ -49,6 +49,29 @@ Berdasarkan nilai `truthy` dan `falsy` diatas mari kita buat contoh lagi
 console.log("" || "bambang"); // bambang
 console.log(true || 0); // bambang
 console.log(undefined || null || ""); // ''
+console.log(undefined || null || "" || 0 || "Hello"); // 'Hello'
 ```
 
 > Ingat! Prinsipnya adalah javascript akan mencari `truthy` value sampai ketemu - kemudian ditampilkan dan selesei, jika tidak ditemukan maka apa yang paling terakhir akan ditampilkan walaupun semuanya `false`.
+
+## Contoh kasus
+
+Kita akan mempelajari lebih jauh dari perilaku _short circuit_ ini pada kasus rumah makan kita. Pertama tama kita akan menggunakan _operator ternary_. Silahkan lihat source codenya disini untuk objek `rumahMakan`
+
+```javascript
+const tanyaMenu = rumahMakan.bakmi ? rumahMakan.bakmi : "Menu tidak ada";
+console.log(tanyaMenu); // 'Menu tidak ada'
+```
+
+> Seperti kita tahu bahwa di objek `rumahMakan` tidak terdapat property 'bakmi' yang membuatnya bernilai `undefined` (falsy value), jadi javascript akan menampilkan kondisi kedua yaitu 'Menu tidak ada'.
+
+Dengan Logika operator `OR` kita dapat mengambil keuntungan dari adanya _short cuircuit_ ini
+
+```javascript
+const tanyaMenu2 = rumahMakan.bakmi || "Menu tidak ada";
+console.log(tanyaMenu2); // 'Menu tidak ada'
+```
+
+> Dengan prinsip yang sama perilaku short ciruit akan mencari nilai kebenaran sampai ketemu. Jika tidak ketemu maka urutan nilai paling terakhir akan di tampilkan
+
+Dan tentu jika `rumahMakan.bakmi` bernilai benar (menunya ada) maka ia akan menjadi `truthy` kemudian akan ditampilkan
--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-// const rumahMakan = {
-//   nama: "Rumah makan berkah",
-//   lokasi: "Jalan merpati blok m",
-//   kategori: ["tradisional", "murah", "warteg"],
-//   makanan: ["soto", "gulai ikan", "rendang", "sate", "nasi goreng"],
-//   minuman: ["es teh", "es jeruk", "kopi"],
-//   jamBuka: {
-//     kam: {
-//       buka: 12,
-//       tutup: 22,
-//     },
-//     jum: {
-//       buka: 11,
-//       tutup: 23,
-//     },
-//     sab: {
-//       buka: 0,
-//       tutup: 24,
-//     },
-//   },
-
-//   mieRebus: function (sawi, ...bumbuLain) {
-//     console.log(sawi);
-//     console.log(...bumbuLain);
-//   },
-// };
-
-/* 3 Sifat mengenai logical operators
-1. Mengembalikan tipe data apapun 
-2. Menggunakan tipe data apapun
-3. Short circuiting evaluation*/
-console.log(undefined || null || ""); // bambang
+// ----- OVERVIEW -----
+console.log(undefined || null || ""); // ""
 console.log("" || "bambang"); // bambang
-console.log(null || undefined);
+console.log(null || undefined); // undefined
+
+// ----- CONTOH KASUS -----
+
+const rumahMakan = {
+  nama: "Rumah makan berkah",
+  lokasi: "Jalan merpati blok m",
+  kategori: ["tradisional", "murah", "warteg"],
+  makanan: ["soto", "gulai ikan", "rendang", "sate", "nasi goreng"],
+  minuman: ["es teh", "es jeruk", "kopi"],
+  jamBuka: {
+    kam: {
+      buka: 12,
+      tutup: 22,
+    },
+    jum: {
+      buka: 11,
+      tutup: 23,
+    },
+    sab: {
+      buka: 0,
+      tutup: 24,
+    },
+  },
+
+  mieRebus: function (sawi, ...bumbuLain) {
+    console.log(sawi);
+    console.log(...bumbuLain);
+  },
+};
+/* Mencari nilai kebenaran dengan operator ternary */
+const tanyaMenu = rumahMakan.bakmi ? rumahMakan.bakmi : "Menu tidak ada";
+console.log(tanyaMenu); // 'Menu tidak ada'
+
+/* Dengan Operator Logika OR */
+const tanyaMenu2 = rumahMakan.bakmi || "Menu tidak ada";
+console.log(tanyaMenu2); // 'Menu tidak ada'
